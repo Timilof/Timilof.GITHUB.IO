ca3f518d98e64ebd027402dcbf688461c8b57fb3
Timilof <zinctim@gmail.com>
2019-02-26 15:21:29 +0100

    routie and transparency fixed

--- week1/public/js/city-refactored.js
+++ week1/public/js/city-refactored.js
@@ -1,62 +1,94 @@
 'use strict'
-const countries = [];
-const template = document.getElementById('main');
+
 
 const router = {
   overview: function() {
-    api.getdata(api.overviewUrl)
-      .then(data => {
-        routie('/', () => {
-          render.elementMake(data)
-          console.log('data is de home')
-        })
-      });
-    routie('/')
-  },
+    routie({
+      '': function() {
+        api.getData(api.overviewUrl)
+          .then(data => {
+            // sections.showMain();
+            render.elementMake(data)
+            console.log('data is home')
+          })
+          .catch(function(error){
+            render.errorpoep()
+          });
+      },
+      'detail/:capital': function() {
+        const currentUrl = document.URL; //Get current url from document
+        const id = currentUrl.substring(currentUrl.lastIndexOf('/') + 1); //Take out the id from url.
 
-  detail: function(id) {
-    // api.getdata(api.detailUrl${id})
-    //   .then(data => {
-// window.location.hash = `detail/${id}`
-    routie('/:capital', data => {
-      render.detailMake(data)
-      console.log('data is de detail')
-      // window.location.hash = 'detail'
+        api.getData(api.detailUrl + id)
+          .then(data => {
+            console.log('data is detail')
+            // sections.showDetail();
+            render.detailMake(data)
+          })
+      }
     })
-  // })
+  }
 }
-}
+// const router = {
+//   overview: function() {
+//     api.getdata(api.overviewUrl)
+//       .then(data => {
+//         routie('/', () => {
+//           render.elementMake(data)
+//           console.log('data is de home')
+//         })
+//       });
+//     routie('/')
+//   },
+//
+//   detail: function() {
+//     const currentUrl = document.URL; //Get current url from document
+//     const id = currentUrl.substring(currentUrl.lastIndexOf('/') + 1); //Take out the id from url.
+//     api.getdata(api.detailUrl+id)
+// console.log(id});
+//     //   .then(data => {
+//     // window.location.hash = `detail/${id}`
+//     routie('detail/:capital', data => {
+//       render.detailMake(data)
+//       console.log('data is de detail')
+//       // window.location.hash = 'detail'
+//     })
+//     // })
+//   }
 
-document.querySelector('#search').addEventListener('input', (e) => {
 
-  const filteredCountries = countries.filter((countries) => countries.toLowerCase().includes(e.target.value.toLowerCase()));
-  template.innerHTML = '';
-  filteredCountries.forEach((countries) => {
-console.log(countries);
-const fixc =
-  `
-<a href="${'#'+(countries.capital)}">
-<section class="template">
-<a href="index.html">Go back!</a>
-<h1 class="naam">${countries.name}</h1>
-<p class="hit">${countries.capital}</p>
-</section>
-</a>
-`
-template.insertAdjacentHTML('beforeend', fixc);
-    // render.elementMake(countries);
-  });
-})
+// document.querySelector('#search').addEventListener('input', (e) => {
+// const template = document.getElementById('main');
+//   const filteredCountries = countries.filter((countries) => countries.toLowerCase().includes(e.target.value.toLowerCase()));
+//   // template.innerHTML = '';
+//   filteredCountries.forEach((countries) => {
+//     console.log(countries);
+//
+//     const saveData = [];
+//     countries.map(country => {
+//       const templateElements = { // create elements for the class and div's.
+//         country: country.name,
+// capital: country.capital,
+//       };
+//       tempData.push(templateElements)
+//
+//       // routie("detail/" + this.capital);
+//     })
+// Transparency.render(template, tempData);
+//     // render.elementMake(countries);
+//   });
+// })
 
 const api = {
-  detailUrl: 'https://restcountries.eu/rest/v2/capital/kabul',
-// function () {
-//            const url = `${api.baseUrl}${this.id}`;
-//            return url;
-//        },
-  overviewUrl : 'https://restcountries.eu/rest/v2/all',
+  countries : [],
+  detailUrl: 'https://restcountries.eu/rest/v2/capital/',
+  // function () {
+  //            const url = `${api.baseUrl}${this.id}`;
+  //            return url;
+  //        },
+  overviewUrl: 'https://restcountries.eu/rest/v2/all',
   baseUrl: 'https://restcountries.eu/rest/v2/',
-  getdata: function(url) {
+  getData: function(url) {
     return new Promise(function(resolve, reject) {
       const request = new XMLHttpRequest();
       request.open('GET', url, true)
@@ -65,9 +97,9 @@ const api = {
           const data = api.parse(request);
           const slicedData = data.slice(0, 52);
           resolve(slicedData);
-          router.detail();
+          // router.detail();
           slicedData.forEach((countryName) => {
-            countries.push(countryName.name);
+            api.countries.push(countryName.name);
           })
         } else {
           reject(request.status);
@@ -86,53 +118,59 @@ const api = {
 
 const render = {
 
-  elementMake: function(json) {
+  elementMake: function(data) {
     // map is used to get only country name and capital form the array
-    json.map(country => {
-      const html =
-        `
-<a href="#/${country.capital}">
-  <section class="template">
-  <h1 class="naam">${country.name}</h1>
-  <p class="hit">${country.capital}</p>
-</section>
-</a>
-  `
-      template.insertAdjacentHTML('beforeend', html);
+    const template = document.getElementById('main');
+    const saveData = [];
+    data.map(country => {
+      const templateElements = { // create elements for the class and div's.
+        country: country.name,
+capital: country.capital,
+      };
+      saveData.push(templateElements)
 
-      // countryLinks.forEach(function(countryLink){
-      // countryLink.addEventListener('click', function(e) {
-      // const id = e.currentTarget.hash.split('#')[1];
-      // router.detail(id);
-      // })
-      // })
+      // routie("detail/" + this.capital);
+    })
+Transparency.render(template, saveData);
+window.localStorage.setItem('country', JSON.stringify(saveData));
+const froot = JSON.parse(window.localStorage.getItem('country'));
+console.log(froot);
+
+    const directives = { // Directives are plain javascript functions defined in a two-dimensional object literal, i.e.,
+      link: {
+        href() {
+          return "#detail/" + this.capital;
+        }
+      }
+    }
+    Transparency.render(template,data, directives, saveData);
+    document.querySelector('.link').addEventListener('click', () => {
+      console.log('this.capital');
     })
   },
 
-  detailMake: function(json) {
-console.log(json);
-    template.innerHTML = '';
-    // map is used to get only country name and capital form the array
-    // json.map(country => {
-      const html =
-        `
-<a href="${'#'+(json.capital)}">
-  <section class="template">
-  <a href="index.html">Go back!</a>
-  <h1 class="naam">${json.name}</h1>
-  <p class="hit">${json.capital}</p>
-</section>
-</a>
-  `
-      template.insertAdjacentHTML('beforeend', html);
+  detailMake: function(data) {
+    // console.log(json);
+    const template = document.getElementById('main');
+    const saveDetailData = [];
+    console.log(data);
+    const currentUrl = document.URL; //Get current url from document
+    const id = currentUrl.substring(currentUrl.lastIndexOf('/') + 1); //Take out the id from url.
+    const countyid = data.filter(county => {
+      return county.capital === id; //if data id is same as id from url return true.
+    });
+    console.log(id);
+    countyid.map(country => {
+      const templateElements = { // create elements for the class and div's.
+        country: country.name,
+        capital: country.capital,
+        // latitude: country.latlng[1],
+      };
+      saveDetailData.push(templateElements)
 
-      // template.addEventListener('click', function() {
-      //   console.log(country.capital);
-      //   routie("detail/" + country.capital);
-      // })
+    })
+    Transparency.render(template, saveDetailData);
 
-    // })
   }
 }
-
-router.overview()
+router.overview();
