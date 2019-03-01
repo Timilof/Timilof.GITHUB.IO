1c6e7790b8c1c990fda6dd33c6ef46f23c01e90e
Timilof <zinctim@gmail.com>
2019-03-01 07:55:56 +0100

    actor diagram and object literal edited

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -5,8 +5,8 @@ const router = {
   // handle : function(){
   // localStoreData.handle();
   // },
-routie: function(){
-  routie({
+  routing: function() {
+    routie({
       '': function() {
         router.mainpage();
       },
@@ -14,77 +14,73 @@ routie: function(){
         router.detailpage()
       },
       'detail': function() {
-              // render.errorPage()
-console.log('prooooooo')
-            }
+        // render.errorPage()
+        console.log('prooooooo')
+      }
     });
-},
-mainpage: function() {
-      render.loader();
-      api.getData(api.overviewUrl)
-        .then(function(data) {
-          render.elementMake(data)
-          render.stopLoader();
-          render.links();
-        })
-        .catch(function(){
-          render.errorPage()
-          render.stopLoader();
-        });
-    },
-    detailpage: function() {
-          render.loader();
-          api.getData(api.overviewUrl)
-            .then(function(data) {
-              render.detailMake(data)
-              render.stopLoader();
-              render.links();
-            })
-            .catch(function(){
-              render.errorPage()
-              render.stopLoader();
-            });
-        }
+  },
+  mainpage: function() {
+    loader.start();
+    api.getData(api.mainUrl)
+      .then(function(data) {
+        render.mainTemplate(data)
+      })
+      .catch(function() {
+        render.errorPage()
+      });
+  },
+  detailpage: function() {
+    loader.start();
+    const id = document.URL.substring(document.URL.lastIndexOf('/') + 1); //Take out the id from url.
+    api.getData(api.detailUrl+id)
+      .then(function(data) {
+        render.detailTemplate(data)
+      })
+      .catch(function() {
+        render.errorPage()
+      });
+  }
 }
 
 const localStoreData = {
   handle: function() {
-          let getLocalstorageData = localStorage.getItem('country');
-          getLocalstorageData = JSON.parse(getLocalstorageData);
+    let getLocalstorageData = localStorage.getItem('country');
+    getLocalstorageData = JSON.parse(getLocalstorageData);
 
-          if (getLocalstorageData){ //If localData has data then use this data
-              render.elementMake(getLocalstorageData);
-              console.log(getLocalstorageData)
-              console.log('Timd')
-              console.log('Timd')
-          }
-          else{              //If localstorage is empty, do a get request
-             router.overview();
-          }
-      },
-      localData: function(data){
-          const saveLocalData = [];
+    if (getLocalstorageData) { //If localData has data then use this data
+      router.mainpage(getLocalstorageData)
+    } else { //If localstorage is empty, do a get request
+      api.getData(api.overviewUrl)
+        .then(function(data) {
+          render.mainTemplate(data)
+        })
+        .catch(function() {
+          render.errorPage()
+        });
+    }
+  },
+  localData: function(data) {
+    const saveLocalData = [];
 
-          data.map(country => { // Map is used to get only an array that contains the title and descriptions of the data from each country.
-              const localElements = { // create elements for the class and div's.
-              country: country.name,
-              capital: country.capital,
-              };
-              saveLocalData.push(localElements)
-                  // console.log(country.name);
-          })
-          window.localStorage.setItem('country', JSON.stringify(saveLocalData));          //Set title and description in localStorage
-      }
+    data.map(country => { // Map is used to get only an array that contains the title and descriptions of the data from each country.
+      const localElements = { // create elements for the class and div's.
+        country: country.name,
+        capital: country.capital,
+      };
+      saveLocalData.push(localElements)
+      // console.log(country.name);
+    })
+    window.localStorage.setItem('country', JSON.stringify(saveLocalData)); //Set title and description in localStorage
+  }
 }
 
 const api = {
-id: function(){
-  const currentUrl = document.URL; //Get current url from document
-  const id = currentUrl.substring(currentUrl.lastIndexOf('/') + 1); //Take out the id from url.
-},
-  countries : [],
+  id: function() {
+    const id = document.URL.substring(document.URL.lastIndexOf('/') + 1); //Take out the id from url.
+  },
+  // countries: [],
   detailUrl: 'https://restcountries.eu/rest/v2/capital/',
-  overviewUrl: 'https://restcountries.eu/rest/v2/all',
+  mainUrl: 'https://restcountries.eu/rest/v2/all',
   getData: function(url) {
     return new Promise(function(resolve, reject) {
       const request = new XMLHttpRequest();
@@ -94,10 +90,9 @@ id: function(){
           const data = api.parse(request);
           const slicedData = data.slice(0, 250);
           resolve(slicedData);
-          // router.detail();
-          slicedData.forEach((countryName) => {
-            api.countries.push(countryName.name);
-          })
+          // slicedData.forEach((countryName) => {
+          //   api.countries.push(countryName.name);
+          // })
         } else {
           reject(request.status);
         }
@@ -113,30 +108,49 @@ id: function(){
   }
 }
 
-const elements = {
 
-}
-
-const render = {
-  loader: function(){
-      const template = document.body;
-      template.classList.remove('hidden');
-      template.classList.add('loadingt');
-},
-stopLoader:function(){
+const loader = {
+  start: function() {
+    const template = document.body;
+    template.classList.remove('hidden');
+    template.classList.add('loadingt');
+  },
+  stop: function() {
     const template = document.body;
     template.classList.remove('loadingt');
     template.classList.add('hidden');
-},
-  elementMake: function(data) {
+  }
+}
+
+const contentStyler ={
+  slimView: function() {
+    const template = document.getElementById('main');
+    template.classList.remove('wide');
+  },
+
+  wideView: function() {
+    const template = document.getElementById('main');
+    template.classList.add('wide');
+  },
+  disabledLinks: function disableLinks() {
+    const inactiveLinks = document.querySelectorAll("a[href='#detail/']");
+    inactiveLinks.forEach((link) => {
+      link.classList.add('inactive');
+      link.href = '#';
+    })
+  }
+}
+
+const render = {
+  mainTemplate: function(data) {
     // map is used to get only country name and capital form the array
     const template = document.getElementById('main');
     const directives = { // Directives are plain javascript functions defined in a two-dimensional object literal, i.e.,
       country: {
-          text(){
+        text() {
           return this.name;
-}
-},
+        }
+      },
       link: {
         href() {
           return "#detail/" + this.capital;
@@ -144,16 +158,17 @@ stopLoader:function(){
       }
     }
     Transparency.render(template, data, directives);
-            render.mainSlim()
+    loader.stop();
+    contentStyler.slimView()
+    contentStyler.disabledLinks()
   },
 
-  detailMake: function(data) {
+  detailTemplate: function(data) {
     // console.log(json);
     const template = document.getElementById('main');
     const saveDetailData = [];
     // console.log(data);
-    const currentUrl = document.URL; //Get current url from document
-    const id = currentUrl.substring(currentUrl.lastIndexOf('/') + 1); //Take out the id from url.
+    const id = document.URL.substring(document.URL.lastIndexOf('/') + 1); //Take out the id from url.
     // console.log()
     const countyid = data.filter(county => {
       return county.capital === decodeURIComponent(id); //if data id is same as id from url return true.
@@ -163,54 +178,37 @@ stopLoader:function(){
       const templateElementsDetail = { // create elements for the class and div's.
         flag: countryDetail.flag,
         country: countryDetail.name,
-        capital: 'capital: '+countryDetail.capital,
-        population: 'population: ' +countryDetail.population,
+        capital: 'capital: ' + countryDetail.capital,
+        population: 'population: ' + countryDetail.population,
         latitude: 'latitude: ' + countryDetail.latlng[1],
-        longtitude: 'longtidude: ' +countryDetail.latlng[0],
+        longtitude: 'longtidude: ' + countryDetail.latlng[0],
       };
       saveDetailData.push(templateElementsDetail)
       const flagg = document.querySelector('.flag');
       flagg.src = templateElementsDetail.flag
-      render.mainWide();
-console.log('detail')
+      contentStyler.wideView();
+      console.log('detail')
     })
 
-
-
+    loader.stop();
     Transparency.render(template, saveDetailData);
 
   },
-mainSlim: function(){
-const template = document.getElementById('main');
-template.classList.remove('wide');
-},
 
-mainWide: function(){
-const template = document.getElementById('main');
-template.classList.add('wide');
-},
-
-errorPage: function(){
-const template = document.getElementById('main');
-const saveErrorElements = [];
-const errorElements = {
-  e_msg: 'Data unavailable',
-  wdid: 'Could not retrieve data from server',
+  errorPage: function() {
+    const template = document.getElementById('main');
+    const saveErrorElements = [];
+    const errorElements = {
+      e_msg: 'Data unavailable',
+      wdid: 'Could not retrieve data from server',
     }
-render.mainWide();
-saveErrorElements.push(errorElements);
-
-Transparency.render(template, saveErrorElements);
-},
-
-links: function disableLinks(){
-const inactiveLinks = document.querySelectorAll("a[href='#detail/']");
-inactiveLinks.forEach((link) => {
-link.classList.add('inactive');
-link.href = '#';
-})
+    contentStyler.wideView();
+    saveErrorElements.push(errorElements);
+    loader.stop();
+    Transparency.render(template, saveErrorElements);
+  }
 }
 
-}
 
-router.routie();
+
+router.routing();
